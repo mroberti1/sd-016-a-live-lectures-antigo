8a1b62e34a28397f3371404c54326c0ba3a07656
Mauricio Moraes Roberti <mroberti@uol.com.br>
2021-09-13 17:55:45 -0300

    Adiciona arquivo Conditional.js - (Condicional.js)

---
+++ lecture-4.1/conditional.js
@@ -0,0 +1,14 @@
+/*
+Saber Mais: https://www.w3schools.com/js/js_if_else.asp
+*/
+
+//Entre { } é o escopo da variável
+let age = 18;
+
+if (age > 18) {
+  console.log('Pode dirigir!');
+} else if (age < 18) {
+  console.log('Não pode dirigir!');
+} else {
+  console.log('Pode dirigir, mas com cuidado! Pois você tem apenas carteira provisória!');
+}
