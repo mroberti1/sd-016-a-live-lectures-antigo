b3ea6977830c1686130187206d438684a5fadf3a
Mauricio Moraes Roberti <mroberti@uol.com.br>
2021-09-13 17:38:05 -0300

    Adiciona o arquivo Arithmetic Operators (Operações Aritméticas)

---
+++ lecture-4.1/arithmetic-operators.js
@@ -0,0 +1,38 @@
+/*
+SABER MAIS!
+https://www.w3schools.com/js/js_arithmetic.asp
+*/
+
+//Declaração de Variável
+//let => tem mutabilidade, pode trocar os valores
+let num1 = 5;
+let num2 = 10;
+let num3 = 15;
+
+//Javascript é um linguagem fracamente tipada
+//Parenteses () serve para proprizar operações, é opcional dependento da operação
+
+//Soma
+let sum = (num1 + num2);
+console.log(sum);
+
+// //Subtração
+// let subtraction = (num1 - num2);
+// console.log(subtraction);
+
+// //Multiplicação
+// let multiplication = (num1 * num2);
+// console.log(multiplication);
+
+// //Divisão
+// let division = (num1 / num2);
+// console.log(division);
+
+// //Operador de Relação
+// let comparation = (num1 > num2);
+// console.log(comparation);
+
+//Como saber se o numero é par?
+//Mod => resto da divisão
+// let isEven = (num1 + num2) % 2 === 0;
+// console.log(isEven);
